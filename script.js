1a2830260058ca41ebfbcd2c87360c5ce642e18a
Sayan Banerjee <90127231+sayan112207@users.noreply.github.com>
2025-03-12 10:45:19 +0530

    Updating progress bar

--- script.js
+++ script.js
@@ -20,10 +20,35 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 
+  // When the user scrolls the page, execute myFunction
+  window.onscroll = function() {myFunction()};
+
+  function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
+
   backToTopButton.addEventListener("click", function() {
     window.scrollTo({
       top: 0,
       behavior: "smooth"
     });
+  });
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute("href"));
+      const headerOffset = document.querySelector("header").offsetHeight;
+      const elementPosition = target.getBoundingClientRect().top + window.scrollY;
+      const offsetPosition = elementPosition - headerOffset;
+
+      window.scrollTo({
+          top: offsetPosition,
+          behavior: "smooth"
+      });
   });
 });
